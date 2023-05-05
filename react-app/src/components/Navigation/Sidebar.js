cbded7f0422456638aea91c05a1871187da45632
Kirin <kirinagcaoili@gmail.com>
2023-05-04 19:46:21 -0700

    changed name of sidebar id color css

--- react-app/src/components/Navigation/Sidebar.js
+++ react-app/src/components/Navigation/Sidebar.js
@@ -4,7 +4,7 @@ function Sidebar() {
     return (
         <div class="container-fluid h-100">
         <div class="row h-100">
-            <div class="col-2" id="green">
+            <div class="col-2" id="yellow">
                 {/* <h4>Sidebar</h4> */}
                 {/* <!-- Navigation links in sidebar--> */}
                 <a href="#">Link 1</a>
